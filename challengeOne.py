eed36db017e5b79c7e87767b42435dce56f62903
Neha Anumalasetty <67026251+nla013neha@users.noreply.github.com>
2020-12-10 17:25:00 -0500

    Create challengeOne.py

---
+++ challengeOne.py
@@ -0,0 +1,6 @@
+# return string as an integer
+# all numbers will be whole & positive
+class stringToInt :
+    stringInput = input("Enter a string: ")
+    print(int(stringInput))
+    print(stringInput + " is an integer!")
